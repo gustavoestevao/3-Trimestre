fa0505ffb0f982ff5d7cbe9820ee871693561f65
gustavoestevao <161479020+gustavoestevao@users.noreply.github.com>
2024-11-05 09:33:53 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lingua ingles',
+    'qual a traduçao da frase today i woke up very upset?',
+    'a traução da frase é hoje eu acordei bem chateado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Italia?',
+    'A capital da Italia é Roma'
+)
+
+criaCartao(
+    'Mercado digital',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
